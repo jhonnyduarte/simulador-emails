bc3e387d60c9af9a91f2dc5736e4e91c1f72108c
Jhon Duarte <jhonny.jdt18@gmail.com>
2025-12-11 07:53:29 +0100

    creando la funcion y mensaje de error

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     //Asignar Eventos
 
@@ -13,10 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function validar(e) {
         if (e.target.value.trim() === ''){
-            console.log('Esta vacio');
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
         } else {
             console.log('No esta vacio');
         }
+    }
+
+    function mostrarAlerta(mensaje) {
+        // Genera la alerta en HTML
+        
+        const error = document.createElement('p');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+        formulario.appendChild(error);
     }
 
 });
