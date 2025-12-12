26683c5343382ca4f6bf61d8290c0e3b19d9b7db
Jhon Duarte <jhonny.jdt18@gmail.com>
2025-12-12 09:48:08 +0100

    validando el campo de email

--- js/app.js
+++ js/app.js
@@ -17,6 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
             return;
         } 
+
+        if(e.target.id === 'email' && !validarEmail(e.target.value)) {
+            mostrarAlerta('El email no es valido', e.target.parentElement)
+            return;
+        }
         limpiarAlerta(e.target.parentElement); 
     }
         
@@ -38,6 +43,13 @@ document.addEventListener('DOMContentLoaded', function() {
         if(alerta) {
             alerta.remove();
         }
+    }
+
+    function validarEmail(email) {
+        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+
+        const resultado = regex.test(email);
+        return resultado;
     }
 
 });
