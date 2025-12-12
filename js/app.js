17da19dc500ad7ffe3b35ecd3e556a48689babd6
Jhon Duarte <jhonny.jdt18@gmail.com>
2025-12-12 08:03:32 +0100

    previniendo alertas multiples y ocultando si pasa la validacion

--- js/app.js
+++ js/app.js
@@ -14,20 +14,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function validar(e) {
         if (e.target.value.trim() === ''){
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
-        } else {
-            console.log('No esta vacio');
-        }
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            return;
+        } 
+        limpiarAlerta(e.target.parentElement); 
     }
-
-    function mostrarAlerta(mensaje) {
-        // Genera la alerta en HTML
         
+
+    function mostrarAlerta(mensaje, referencia) {
+        
+        limpiarAlerta(referencia);
+        // Genera la alerta en HTML
         const error = document.createElement('p');
         error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
 
-        formulario.appendChild(error);
+        referencia.appendChild(error);
+    }
+
+    function limpiarAlerta(referencia) {
+        //Comprueba que ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta) {
+            alerta.remove();
+        }
     }
 
 });
